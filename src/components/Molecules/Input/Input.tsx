77376ac3742eae6c61c5051601b7fc538c993d81
Tiago Cintra <tiago.slv@hotmail.com>
2023-08-24 09:54:40 -0300

    refactor(input): forwardref added to input
    improvement to use rhf lib with custom component

--- src/components/Molecules/Input/Input.tsx
+++ src/components/Molecules/Input/Input.tsx
@@ -1,5 +1,5 @@
+import { MouseEventHandler, ReactNode, forwardRef } from 'react'
 import clsx from 'clsx'
-import { MouseEventHandler, ReactNode } from 'react'
 
 interface InputProps
   extends React.DetailedHTMLProps<
@@ -15,58 +15,63 @@ interface InputProps
   onRightElementClick?: MouseEventHandler
 }
 
-export const Input = ({
-  type,
-  label,
-  labelColor = 'black',
-  border = 'gray',
-  className,
-  rightElement,
-  onRightElementClick,
-  ...inputprops
-}: InputProps) => {
-  const inputBorder = {
-    gray: 'ring-gray-500',
-    purple: 'ring-1-900',
-    none: 'ring-transparent',
-  }
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({
+    type,
+    label,
+    labelColor = 'black',
+    border = 'gray',
+    className,
+    rightElement,
+    onRightElementClick,
+    ...inputprops
+  }: InputProps, ref) => {
+    const inputBorder = {
+      gray: 'ring-gray-500',
+      purple: 'ring-1-900',
+      none: 'ring-transparent',
+    }
 
-  return (
-    <div className={clsx('flex w-full flex-col gap-2 font-prim', className)}>
-      <label
-        htmlFor={inputprops.name}
-        className={clsx('text-sm', { 'text-white': labelColor === 'white' })}
-      >
-        {label}
-      </label>
-      <div
-        //TODO: verificar com equipe de UI/UX o padrão de outline
-        className={clsx(
-          'box-content flex max-w-full items-center justify-center overflow-hidden rounded bg-white ring-1 ring-inset',
-          inputBorder[border],
-          'p-1 transition focus-within:shadow-border focus-within:ring-black'
-        )}
-      >
-        <input
-          type={type}
-          name={inputprops.name}
-          id={inputprops.name}
-          placeholder={inputprops.placeholder}
+    return (
+      <div className={clsx('flex w-full flex-col gap-2 font-prim', className)}>
+        <label
+          htmlFor={inputprops.name}
+          className={clsx('text-sm', { 'text-white': labelColor === 'white' })}
+        >
+          {label}
+        </label>
+        <div
+          //TODO: verificar com equipe de UI/UX o padrão de outline
           className={clsx(
-            'w-full py-2 pl-4 font-prim text-base tracking-wide outline-none placeholder:text-xs',
-            { 'pr-4': !rightElement }
+            'box-content flex max-w-full items-center justify-center overflow-hidden rounded bg-white ring-1 ring-inset',
+            inputBorder[border],
+            'p-1 transition focus-within:shadow-border focus-within:ring-black'
           )}
-          {...inputprops}
-        />
-        {rightElement && (
-          <button
-            className="rounded px-4 outline-none ring-2 ring-transparent transition duration-200 focus-visible:shadow-border focus-visible:ring-black"
-            onClick={onRightElementClick}
-          >
-            {rightElement}
-          </button>
-        )}
+        >
+          <input
+            type={type}
+            name={inputprops.name}
+            id={inputprops.name}
+            placeholder={inputprops.placeholder}
+            className={clsx(
+              'w-full py-2 pl-4 font-prim text-base tracking-wide outline-none placeholder:text-xs',
+              { 'pr-4': !rightElement }
+            )}
+            ref={ref}
+            {...inputprops}
+          />
+          {rightElement && (
+            <button
+              className="rounded px-4 outline-none ring-2 ring-transparent transition duration-200 focus-visible:shadow-border focus-visible:ring-black"
+              onClick={onRightElementClick}
+            >
+              {rightElement}
+            </button>
+          )}
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+)
+
+Input.displayName = 'Input'
