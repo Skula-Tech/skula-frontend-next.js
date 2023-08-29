f178cbd2db3d914054fddf2b0703ea5ab060018a
Tiago Cintra <tiago.slv@hotmail.com>
2023-08-29 14:18:43 -0300

    refactor(homeheader): organism component refactored to receive tv slots

--- src/components/Organisms/HomeHeader/HomeHeader.tsx
+++ src/components/Organisms/HomeHeader/HomeHeader.tsx
@@ -1,20 +1,30 @@
 import Image from 'next/image'
 import SkulaLogo from '@/assets/logos/skula-logo.png'
+import { tv } from 'tailwind-variants'
 import { MobMenu, SearchNormal } from '@/assets/icons'
 import { Heading, Text } from '@/components/Atoms'
 import { Input } from '@/components/Molecules'
 
+const header = tv({
+  slots: {
+    base: 'flex h-full w-full flex-col items-center gap-6 bg-gradient-lg px-4 py-6',
+    head: 'flex w-full items-center justify-between gap-4',
+  },
+})
+
+const { base, head } = header()
+
 export const HomeHeader = () => {
   return (
-    <header className="flex h-full w-full flex-col items-center gap-6 bg-gradient-lg px-4 py-6">
-      <div className="flex w-full items-center justify-between gap-4">
-        <button className="">
+    <header className={base()}>
+      <div className={head()}>
+        <button>
           <MobMenu />
         </button>
         <Image src={SkulaLogo} alt="Skula Logo" />
         <div className="w-11" />
       </div>
-      <Heading fontStyle="xxl" className="text-[#E9E9E9]">
+      <Heading as="h1" className="text-[#e9e9e9]">
         Bora aprender?
       </Heading>
       <Text className="text-center text-neutral-50">
